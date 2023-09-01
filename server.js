7cfb08d879b498abccdffa3b5d5dfcc7486cbb5a
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-09-01 15:51:01 -0700

    Add rum agent

--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 const express = require("express");
 const path = require("path");
 
+import { AwsRum } from "aws-rum-web";
+
+try {
+  const config = {
+    sessionSampleRate: 0.1,
+    endpoint: "https://dataplane.rum.us-west-2.amazonaws.com",
+    telemetries: [],
+    allowCookies: false,
+    enableXRay: false,
+  };
+
+  const APPLICATION_ID = "7ee76bc6-276f-4497-834b-1df09ff6313b";
+  const APPLICATION_VERSION = "1.0.0";
+  const APPLICATION_REGION = "us-west-2";
+
+  const awsRum = new AwsRum(
+    APPLICATION_ID,
+    APPLICATION_VERSION,
+    APPLICATION_REGION,
+    config
+  );
+} catch (error) {
+  // Ignore errors thrown during CloudWatch RUM web client initialization
+}
+
 const app = express();
 
 // Serve the build folder
