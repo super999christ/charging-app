f6d43277e90da9ef60a5bc132176379aa3051ac2
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-03 12:24:41 -0700

    Setup import meta env to dynamically load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import { resolve } from "path";
 import AutoImport from "unplugin-auto-import/vite";
 import svgr from "vite-plugin-svgr";
+import ImportMetaEnvPlugin from "@import-meta-env/unplugin";
 
 const host = process.env.HOST || true;
 const port = +(process.env.PORT || 80);
 const projectRoot = resolve(__dirname, "app");
 
-export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
+export default defineConfig(() => {
   return {
     root: "./app",
     server: { host, port },
@@ -22,13 +21,15 @@ export default defineConfig(({ command, mode }) => {
         imports: ["vitest"],
         dts: true, // generate Typescript definitions
       }),
+      ImportMetaEnvPlugin.vite({
+        example: ".env.example",
+        env: ".env",
+        transformMode: "runtime",
+      }),
     ],
     build: {
       outDir: "../build",
       sourcemap: true,
-    },
-    define: {
-      "process.env": env,
     },
     resolve: {
       alias: {
