b69c38810a2d7b1ffadc019ae7a8f3bef370a247
Drioux Guidry <driouxguidry@nxuenergy.com>
2023-10-13 10:30:22 -0700

    Fix auth hook auto routing with invalid token

--- app/hooks/useAuth.ts
+++ app/hooks/useAuth.ts
@@ -1,14 +1,11 @@
-import { useEffect } from "react";
 import { validateToken } from "@root/validations";
-import { useNavigate } from "react-router";
 
 export default function useAuth() {
-  const navigate = useNavigate();
-
   const isTokenValid = validateToken();
-  useEffect(() => {
-    if (!isTokenValid) navigate("/");
-  }, [isTokenValid]);
+
+  if (!isTokenValid) {
+    window.location.href = "/";
+  }
 
   return {};
 }
